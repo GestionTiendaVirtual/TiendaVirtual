acf6cd9bc125a8bb8175c517ffdd27c8787f819e
Michael <michael.melendezm@gmail.com>
2017-02-20 13:34:07 -0600

    20/02/2017

--- Presentation/Modules/ClientView.php
+++ Presentation/Modules/ClientView.php
@@ -4,11 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="../../CSS/menu.css" rel="stylesheet" type="text/css"/>
-        <title></title>
-
-
-
-
+        <title>MGA Soluciones</title>
         <!-- ******************* Para Busquedas ******************* -->
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
         <script src="//code.jquery.com/jquery-1.10.2.js"></script>
@@ -167,7 +163,7 @@
                     <?php
                     $cont = 0;
                     foreach ($currentProducts->getPathImages() as $path) {
-                        if ($cont < 3) {
+                        if ($cont < 4) {
                             ?>
                             <td><a href="../Product/ProductDetail.php?idProduct=<?php echo $currentProducts->getIdProduct(); ?>">
                                     <img class="zoom"style="width: 100px; height: 100px;"src="<?php echo $path; ?>"></a>&emsp;&emsp;</td>
