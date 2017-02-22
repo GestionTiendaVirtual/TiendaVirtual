f9d3c8cf4d67ae47d25b5cd4a10aa7442dd1f544
Michael <michael.melendezm@gmail.com>
2017-02-22 10:22:18 -0600

    22/02/2017

--- Data/RecoverPasswordData.php
+++ Data/RecoverPasswordData.php
@@ -2,6 +2,7 @@
 
 include_once 'Data.php';
 require_once '../../Resources/PHPMailer/PHPMailerAutoload.php';
+
 /**
  * Description of RecoverPasswordData
  *
@@ -19,23 +20,25 @@ class RecoverPasswordData extends Data {
         mysqli_close($conn);
         $email = $rowEmail[0];
         if (strlen($email)) {
-                
+
             $mail = new PHPMailer();
             $mail->isSMTP();
+            $mail->SMTPDebug = 2;
+            $mail->Debugoutput = 'html';
             $mail->Host = 'smtp.gmail.com';
             $mail->Port = 587;
             $mail->SMTPSecure = 'tls';
             $mail->SMTPAuth = true;
-            $mail->Username = "gymcaobacr@gmail.com";
-            $mail->Password = "gymcaoba12345";            
-            $mail->setFrom('gymcaobacr@gmail.com', 'MGA Soluciones');
-            $mail->addAddress('michael.melendezm@gmail.com', 'MGA Store');
-            $mail->Subject = 'Solicitud de contraseña';
-            $message = "Su contraseña temporal es: ";
+            $mail->Username = "mgasoluciones17@gmail.com";
+            $mail->Password = "adminMGA";
+            $mail->setFrom('mgasoluciones17@gmail.com', 'MGA Store');
+            $mail->addAddress('michael.melendezm@gmail.com', 'Michael');
+            $mail->Subject = 'Solicitud recuperar contraseña';
+            $message = "Su contraseña temporal es ";
             $mail->msgHTML($message);
-            $exito = $mail->send();
+            $success = $mail->send();
 
-            if ($exito) {
+            if ($success) {
                 return true;
             } else {
                 return false;
